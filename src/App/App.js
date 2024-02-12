3acea923520cc802d5fab3b03f4a82f7a9ae8631
austin-michael <austin.michaelwill@gmail.com>
2024-02-12 11:58:27 -0700

    Created Camera and Renderer

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import * as THREE from "three";
+import Camera from "./Camera";
+import Renderer from "./Renderer";
+
+let instance = null;
+
+export default class App {
+  constructor() {
+    if (instance) return instance;
+    instance = this;
+
+    this.canvas = document.querySelector("canvas.threejs");
+    this.scence = new THREE.Scene();
+    this.camera = new Camera();
+    this.renderer = new Renderer();
+  }
+}
