46c9b4bece30bcf799b4aa5d53aacd3a267c986c
austin-michael <austin.michaelwill@gmail.com>
2024-02-13 11:23:16 -0700

    Added physics for cubes, spheres, and trimeshes

--- src/App/World/World.js
+++ src/App/World/World.js
@@ -1,24 +1,31 @@
-import * as THREE from 'three'
+import * as THREE from "three";
 
-import App from '../App.js'
-import Physics from './Physics.js'
-import Environment from './Environment.js'
+import App from "../App.js";
+import Physics from "./Physics.js";
+import Environment from "./Environment.js";
+import Character from "./Character.js";
 
-export default class World{
-    constructor() {
-        this.app = new App()
-        this.scene = this.app.scene
+import { appStateStore } from "../Utils/Store.js";
 
-        // create world classes
-        this.physics = new Physics()
-        this.environment = new Environment()
+export default class World {
+  constructor() {
+    this.app = new App();
+    this.scene = this.app.scene;
 
-        this.loop()
-    }
+    this.physics = new Physics();
 
+    // create world classes
+    appStateStore.subscribe((state) => {
+      if (state.physicsReady) {
+        this.environment = new Environment();
+        this.character = new Character();
+      }
+    });
 
+    this.loop();
+  }
 
-    loop(deltaTime, elapsedTime) {
-        this.physics.loop()
-    }
+  loop(deltaTime, elapsedTime) {
+    this.physics.loop();
+  }
 }
