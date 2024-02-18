20d0a2b692a6543f43851794177e75a51795a160
austin-michael <austin.michaelwill@gmail.com>
2024-02-18 11:07:27 -0700

    Removing all character, animation, portal, and phsyics code to prepare for new scene

--- src/App/World/World.js
+++ src/App/World/World.js
@@ -1,11 +1,7 @@
 import * as THREE from "three";
 
 import App from "../App.js";
-import Physics from "./Physics.js";
 import Environment from "./Environment.js";
-import Character from "./Character.js";
-import CharacterController from "./CharacterController.js";
-import AnimationController from "./AnimationController.js";
 
 import { appStateStore } from "../Utils/Store.js";
 
@@ -13,16 +9,11 @@ export default class World {
   constructor() {
     this.app = new App();
     this.scene = this.app.scene;
-    
-    this.physics = new Physics();
 
     // create world classes
     const unsub = appStateStore.subscribe((state) => {
-      if (state.physicsReady && state.assetsReady) {
+      if (state.assetsReady) {
         this.environment = new Environment();
-        this.character = new Character();
-        this.characterController = new CharacterController();
-        this.animationController = new AnimationController();
         unsub();
       }
     });
@@ -31,9 +22,6 @@ export default class World {
   }
 
   loop(deltaTime, elapsedTime) {
-    this.physics.loop();
-    if(this.environment) this.environment.loop();
-    if(this.characterController) this.characterController.loop();
-    if(this.animationController) this.animationController.loop(deltaTime);
+    if (this.environment) this.environment.loop();
   }
 }
